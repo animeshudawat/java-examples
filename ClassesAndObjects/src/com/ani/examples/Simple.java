3b3e91a3054edde9eb597fc4b29e18426a4a9e2e
Animesh Udawat <animeshudawat@gmail.com>
2021-03-11 16:57:58 +0530

    Added a simple class
     Author:    Animesh Udawat <animeshudawat@gmail.com>

---
+++ ClassesAndObjects/src/com/ani/examples/Simple.java
@@ -0,0 +1,12 @@
+package com.ani.examples;
+
+public class Simple {
+	//Instance Variables
+	int a =  10;
+	String str = "animesh";
+	
+	public void printAll() {
+		System.out.println(str + " " + a);
+	}
+
+}
