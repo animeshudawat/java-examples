0e9e5c5b9fc6b3ad5c9f03c692ca7b3bb2623264
Animesh Udawat <animeshudawat@gmail.com>
2021-03-26 21:40:10 +0530

    Added classloader way of creating objects

--- ClassesAndObjects/src/com/ani/examples/ObjectInitialization.java
+++ ClassesAndObjects/src/com/ani/examples/ObjectInitialization.java
@@ -1,8 +1,21 @@
 package com.ani.examples;
 
+class AnotherClass {
+	public AnotherClass() {
+		System.out.println("Object of simple got created");
+	}
+	
+	public void simpleMethod() {
+		System.out.println("I am a simple method");
+	}
+}
+
 public class ObjectInitialization {
 	
 	private int a;
+	public static void staticMethod() {
+		System.out.println("I am a static method");
+	}
 	
 	public ObjectInitialization(int a) {
 		this.a = a;
@@ -12,7 +25,7 @@ public class ObjectInitialization {
 		System.out.println(this.a * n);
 	}
 	
-	public static void main(String a[]) {
+	public static void main(String a[]) throws InstantiationException, IllegalAccessException {
 		//By constructor
 		ObjectInitialization obj_1 = new ObjectInitialization(5);
 		obj_1.muliply(2);
@@ -24,5 +37,16 @@ public class ObjectInitialization {
 		ObjectInitialization obj_2 = new ObjectInitialization(4), obj_3 = new ObjectInitialization(5);
 		obj_2.muliply(10);
 		obj_3.muliply(3);	
+		
+		//through class loader
+		try {
+			Class simpleClass = Class.forName("com.ani.examples.AnotherClass");
+			AnotherClass obj = (AnotherClass)simpleClass.newInstance();
+			obj.simpleMethod();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
